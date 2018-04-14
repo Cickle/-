146b3025143a049c4543b6aae1949e4d1f8c707e
cickle <215009924@qq.com>
2018-04-14 08:45:31 +0800

    说明1

--- 批量处理文件/Program.cs
+++ 批量处理文件/Program.cs
@@ -12,6 +12,17 @@ namespace 批量处理文件
         {
             Console.WriteLine("批量处理文件程序");
             Console.WriteLine("1.将目标文件夹复制到指定目录中的每个文件夹中");
+            string a = Console.ReadLine();
+            switch (a)
+            {
+                case "1":
+                    CopyFile();
+                    break;
+            }
+        }
+        static void CopyFile()
+        {
+
         }
     }
 }
